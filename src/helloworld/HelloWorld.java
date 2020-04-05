b0e76a5d4bc0d41568a1cd348b6ce8c02775605b
张轲 <@DESKTOP-SQH9Q4H>
2020-04-05 13:49:50 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world 随便改一改");
+		System.out.println("hello world 随便改");
 	}
 }
